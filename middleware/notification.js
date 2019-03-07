ee255480e218cd7b6514c5df7712c34ff0e61715
Nethanel Kohen <nethanel.kohen@gmail.com>
2019-03-07 17:31:07 -0500

    added notification feature

--- middleware/notification.js
+++ middleware/notification.js
@@ -1,58 +1,98 @@
 const { Expo } = require("expo-server-sdk");
-const { User } = require("../models");
+const { User, Station } = require("../models");
 const expo = new Expo();
 
-let savedPushTokens = [];
-
-const handlePushTokens = arg => {
+const handlePushTokens = async arg => {
   let { train, station, status_update } = arg;
 
-  // Create the messages that you want to send to clents
   let notifications = [];
-  for (let pushToken of savedPushTokens) {
-    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
 
-    // Check that all your push tokens appear to be valid Expo push tokens
-    if (!Expo.isExpoPushToken(pushToken)) {
-      console.error(`Push token ${pushToken} is not a valid Expo push token`);
-      continue;
-    }
+  getUsers = () => {
+    return User.findAll({
+      where: {
+        notifications_setting: 1,
+        home_station: station
+      }
+    }).then(res => res);
+  };
 
-    // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
-    notifications.push({
-      to: "ExponentPushToken[z1S5H1CSP3kJzwtKBc3Y1j]",
-      sound: "default",
-      title: "Message received!",
-      body: "TEST"
+  let users = await getUsers();
+
+  if (users) {
+    users.map(selectUser => {
+      let { expo_token } = selectUser.dataValues;
+      if (Expo.isExpoPushToken(expo_token)) {
+        notifications.push({
+          to: expo_token,
+          sound: "default",
+          title: `Update at ${station}`,
+          body: `The ${train} has been updated to: ${status_update}`
+        });
+      } else return;
     });
   }
-  notifications.push({
-    to: "ExponentPushToken[z1S5H1CSP3kJzwtKBc3Y1j]",
-    sound: "default",
-    title: `Update at ${station}`,
-    body: `The ${train} has been updated to: ${status_update}`
-  });
 
-  // The Expo push notification service accepts batches of notifications so
-  // that you don't need to send 1000 requests to send 1000 notifications. We
-  // recommend you batch your notifications to reduce the number of requests
-  // and to compress them (notifications with similar content will get
-  // compressed).
   let chunks = expo.chunkPushNotifications(notifications);
 
   (async () => {
-    // Send the chunks to the Expo push notification service. There are
-    // different strategies you could use. A simple one is to send one chunk at a
-    // time, which nicely spreads the load out over time:
     for (let chunk of chunks) {
       try {
         let receipts = await expo.sendPushNotificationsAsync(chunk);
-        console.log(receipts);
+        console.log("RECEIPTS:", receipts);
       } catch (error) {
-        console.error(error);
+        console.error("ERROR FROM EXPO:", error);
       }
     }
   })();
 };
 
 module.exports.handlePushTokens = handlePushTokens;
+
+// const handlePushTokensMorning = async arg => {
+//   let notifications = [];
+
+//   getUsers = () => {
+//     return User.findAll({
+//       where: {
+//         notifications_setting: 1
+//       }
+//     }).then(res => res);
+//   };
+
+//   let users = await getUsers();
+
+//   getStations = () => {
+//     return Station.findAll().then(res => res.map(stat => stat.dataValues));
+//   };
+
+//   let stations = await getStations();
+
+//   if (users) {
+//     users.map(selectUser => {
+//       let { expo_token, home_station } = selectUser.dataValues;
+//       if (Expo.isExpoPushToken(expo_token)) {
+//         notifications.push({
+//           to: expo_token,
+//           sound: "default",
+//           title: `${home_station} `,
+//           body: `The ${train} has been updated to: ${status_update}`
+//         });
+//       } else return;
+//     });
+//   }
+
+//   let chunks = expo.chunkPushNotifications(notifications);
+
+//   (async () => {
+//     for (let chunk of chunks) {
+//       try {
+//         let receipts = await expo.sendPushNotificationsAsync(chunk);
+//         console.log("RECEIPTS:", receipts);
+//       } catch (error) {
+//         console.error("ERROR FROM EXPO:", error);
+//       }
+//     }
+//   })();
+// };
+
+// module.exports.handlePushTokensMorning = handlePushTokensMorning;
--- routes/token.js
+++ routes/token.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("../models");
-
-const { handlePushTokens } = require("../middleware/notification");
+const {
+  handlePushTokens,
+  handlePushTokensMorning
+} = require("../middleware/notification");
 
 router.post("/:id", (req, res) => {
   const { token } = req.body.token;
@@ -14,6 +16,8 @@ router.post("/:id", (req, res) => {
 
   return res.send(`Updated user's token`);
 });
+
+router.get("/morning", (req, res) => handlePushTokensMorning());
 
 router.post("/message", (req, res) => {
   handlePushTokens(req.body.message);
